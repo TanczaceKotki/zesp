4e41e4dab9e2938aef115fd253d7d8d9d3dacc44
Jakub Brąz <jakub.braz@uj.edu.pl>
2015-04-29 21:15:26 +0200

    Walidacja adresu email #2

--- Formularze/test_walidacja.php
+++ Formularze/test_walidacja.php
@@ -4,8 +4,10 @@
 	$email1 = "jan.kowalski@gmail.com";
 	$email2 = "1jankowalski@o2.pl";
 	$email3 = "moj_adres@com";
+	$email4 = "jakub.braz@uj.edu.pl";
 	
 	echo (valid_email( $email1 ) == true).PHP_EOL;
 	echo (valid_email( $email2 ) == false).PHP_EOL;
 	echo (valid_email( $email3 ) == false).PHP_EOL;
+	echo (valid_email( $email4 ) == true).PHP_EOL;
 ?>
--- Formularze/walidacja.php
+++ Formularze/walidacja.php
@@ -1,3 +1,15 @@
 <?php
-echo "test";
+
+	function valid_email( $email ){
+		$arg = strtolower( $email );
+		// $expression = '/^[a-z]{1}[a-z0-9\.]*@\.[a-z]+$/';
+		$expression = '/^[a-z][a-z0-9\._]*@[a-z\.]+\.[a-z\.]+$/';
+
+		if( preg_match( $expression, $arg ) ){
+			return true;
+		}
+
+		return false;
+	}
+
 ?>
