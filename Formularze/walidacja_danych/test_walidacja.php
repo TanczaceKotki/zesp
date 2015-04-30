2b61900210315882a598af444caaf9aa366b2dbc
Jakub Brąz <jakub.braz@uj.edu.pl>
2015-04-30 05:53:29 +0200

    Walidacja danych PHP

--- Formularze/walidacja_danych/test_walidacja.php
+++ Formularze/walidacja_danych/test_walidacja.php
@@ -1,13 +1,69 @@
 <?php
 	require 'walidacja.php';
 
+	function test_equal( $val1, $val2 ){
+		$is_equal = ( $val1 == $val2 );
+		if( $is_equal == true ){
+			echo "TEST OK".PHP_EOL;
+		}
+		else{
+			echo "TEST ERROR".PHP_EOL;
+		}
+	}
+
+	echo "TESTY FUNKCJI valid_length"."\n";
+	$max_length = 10;
+	$napis = "abc";
+	test_equal( valid_length( $napis, $max_length ), true );
+	$napis = "abcdefghij";
+	test_equal( valid_length( $napis, $max_length ), true );
+	$napis = "abcdefghijk";
+	test_equal( valid_length( $napis, $max_length ), false );
+
+	echo "TEST FUNKCJI valid_email"."\n";
+	$max_length = 50;
 	$email1 = "jan.kowalski@gmail.com";
 	$email2 = "1jankowalski@o2.pl";
 	$email3 = "moj_adres@com";
 	$email4 = "jakub.braz@uj.edu.pl";
-	
-	echo (valid_email( $email1 ) == true).PHP_EOL;
-	echo (valid_email( $email2 ) == false).PHP_EOL;
-	echo (valid_email( $email3 ) == false).PHP_EOL;
-	echo (valid_email( $email4 ) == true).PHP_EOL;
+	$email5 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@uj.edu.pl";
+	test_equal( valid_email( $email1, $max_length ), true );
+	test_equal( valid_email( $email2, $max_length ), false );
+	test_equal( valid_email( $email3, $max_length ), false );
+	test_equal( valid_email( $email4, $max_length ), true );
+	test_equal( valid_email( $email5, $max_length ), false );
+
+	echo "TEST FUNKCJI valid_first_name\n";
+	$max_length = 20;
+	$name = "Kuba";
+	test_equal( valid_first_name( $name, $max_length ), true );
+	$name = "kuba";
+	test_equal( valid_first_name( $name, $max_length ), false );
+	$name = "Coś";
+	test_equal( valid_first_name( $name, $max_length ), true );
+	$name = "Ściana";
+	test_equal( valid_first_name( $name, $max_length ), false );
+	$name = "Kubaaaaaaaaaaaaaaaaaa";
+	test_equal( valid_first_name( $name, $max_length ), false );
+	$name = "Ku_ba";
+	test_equal( valid_first_name( $name, $max_length ), false );
+	$name = "K";
+	test_equal( valid_first_name( $name, $max_length ), false );
+
+	echo "TEST FUNKCI valid_second_name\n";
+	$max_length = 20;
+	$name = "Brąz";
+	test_equal( valid_second_name( $name, $max_length ), true );
+	$name = "Brąz-Kowalski";
+	test_equal( valid_second_name( $name, $max_length ), true );
+	$name = "Kowalski_Nowak";
+	test_equal( valid_second_name( $name, $max_length ), false );
+	$name = "Nowakowski";
+	// test_equal( valid_second_name( $name, $max_length ), true );
+	// $name = "Ściana";
+	test_equal( valid_second_name( $name, $max_length ), true );
+	$name = "kowalski";
+	test_equal( valid_second_name( $name, $max_length ), false );
+	$name = "Jan-Maria";
+	test_equal( valid_second_name( $name, $max_length ), true );
 ?>
--- Formularze/walidacja_danych/walidacja.php
+++ Formularze/walidacja_danych/walidacja.php
@@ -1,15 +1,36 @@
 <?php
+	
+	function valid_length( $arg, $max_length ){
+		return ( strlen( $arg ) <= $max_length );
+	}
 
-	function valid_email( $email ){
-		$arg = strtolower( $email );
-		// $expression = '/^[a-z]{1}[a-z0-9\.]*@\.[a-z]+$/';
-		$expression = '/^[a-z][a-z0-9\._]*@[a-z\.]+\.[a-z\.]+$/';
-
-		if( preg_match( $expression, $arg ) ){
-			return true;
+	function valid_email( $email, $max_length){
+		if( valid_length($email, $max_length) == false ){
+			return false;
 		}
 
-		return false;
+		$arg = strtolower( $email );
+		$expression = '/^[a-z][a-z0-9\._]*@[a-z\.]+\.[a-z\.]+$/';
+
+		return preg_match( $expression, $arg );
+	}
+
+	function valid_first_name( $name, $max_length ){
+		if( valid_length( $name, $max_length ) == false ){
+			return false;
+		}
+
+		$expression = '/^[A-Z][a-zęółśążźćń]+$/';
+		return preg_match( $expression, $name );
+	}
+
+	function valid_second_name( $name, $max_length ){
+		if( valid_length( $name, $max_length ) == false ){
+			return false;
+		}
+
+		$expression = '/^[A-ZŁŚŹĆŻ][a-zęółśążźćń]*(-[A-ZŁŚŹĆŻ][a-zęółśążźćń]*){0,1}$/';
+		return preg_match( $expression, $name );
 	}
 
 ?>
