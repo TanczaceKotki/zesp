74333203c5e5ce1a8b91924d5f9e41f15bdc8977
CorneliusMaximus <mat.kornecki@gmail.com>
2015-05-18 23:39:43 +0200

    Walidacja zdjęć w JS

--- Formularze/view_zdjecie.php
+++ Formularze/view_zdjecie.php
@@ -3,41 +3,12 @@
 	require 'DB.php';
 	top();
 	$DB=dbconnect();
-	if(isset($_POST['submitted'])){
-		$send=False;
-		$params=array();
-		$sql='UPDATE Zdjecie SET';
-		if($_POST['link']!==$_POST['old_link']){
-			$sql.=' link=?';
-			$params[]=$_POST['link'];
-			$send=True;
-		}
-		if($_POST['sprzet']!==$_POST['old_sprzet']){
-			if($send) $sql.=',';
-			$sql.=' sprzet=?';
-			$params[]=$_POST['sprzet'];
-			$send=True;
-		}
-		if($send){
-			$sql.=' WHERE id=?';
-			$params[]=$_POST['id'];
-			if($st=$DB->prepare($sql)){
-				if($st->execute($params)) echo 'Zdjęcie zostało pomyślnie zmodyfikowane.<br /><br />';
-				else echo 'Nastąpił błąd przy modyfikowaniu zdjęcia: '.implode(' ',$st->errorInfo()).'<br /><br />';
-			}
-			else echo 'Nastąpił błąd przy modyfikowaniu zdjęcia: '.implode(' ',$DB->errorInfo()).'<br /><br />';
-		}
-	}
 	if($st=$DB->prepare('SELECT * FROM Zdjecie WHERE id=?')){
 		if($st->execute(array($_GET['id']))){
 			if($row=$st->fetch(PDO::FETCH_ASSOC)){
 				?><form action="index.php" method="POST" accept-charset="UTF-8" enctype="application/x-www-form-urlencoded">
 					<input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
 					<input type="submit" name="del_zdjecie" value="Usuń" />
-				</form>
-				<form action="edit_zdjecie.php?=<?php echo $row['id']; ?>" method="POST" accept-charset="UTF-8" enctype="application/x-www-form-urlencoded">
-					<input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
-					<input type="submit" value="Edytuj" />
 				</form>
 				<br />
 				<table>
