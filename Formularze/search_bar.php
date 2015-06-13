7713d8169a239367c3a1e204d3e14d7d9064e9f7
bananek <bananeeek@gmail.com>
2015-06-13 17:44:43 +0200

    Update search_bar.php

--- Formularze/search_bar.php
+++ Formularze/search_bar.php
@@ -1,192 +1,171 @@
-<!DOCTYPE>
-<html>
-
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<title>Wyszukiwarka</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+				<style>table,tr,td{border:1px solid #000}</style>
+	</head>
 <body>
 
-
 <div align="left">
+
 Kategoria:<br>
 
-<form  method="post" action="search.php?go"  id="searchform">
+<form  method="post" action="search_bar.php"  id="searchform">
 
-<select name="category">
-<option value="Sprzet">Sprzęt</option>
-<option value="Osoba">Osoba</option>
-<option value="Laboratorium">Laboratorium</option>
-<option value="Tag">Tag</option>
-<option value="Zespol">Zespół</option>
-<option value="Zaklad">Zakład</option>
-<option value="Projekt">Projekt</option>
-</select>
-<br>
+	<select name="category">
+		<option value="Sprzet">Sprzęt</option>
+		<option value="Osoba">Osoba</option>
+		<option value="Laboratorium">Laboratorium</option>
+		<option value="Tag">Tag</option>
+		<option value="Zespol">Zespół</option>
+		<option value="Zaklad">Zakład</option>
+		<option value="Projekt">Projekt</option>
+	</select>
+	<br>
 
-<input  type="text" name="keyword">
-<input  type="submit" name="submit" value="Search">
+	<input  type="text" name="keyword">
+	<input  type="submit" name="submit" value="Search">
+
 </form>
 
+</div>
 
-
-
+<div align="center">
 <?php
 
+require 'common.php';
+require 'DB.php';
+top();
+$DB=dbconnect();
 
-  if(isset($_POST['submit'])){
-  	if(isset($_GET['go'])){
-  		if(preg_match("/^\d[ a-zA-Z]+/", $_POST['keyword'])){
-		
-
- 			 $keyword=$_POST['keyword'];
-			 $cat=$_POST['category'];
-			 $DBName = nazwadb; //do zmiany
-
-
-  			//połączenie z bazą danych, TRZEBA ZMIENIĆ PARAMETRY!!
-  			$db=mysql_connect  ("servername", "username",  "password") or die ('Nie można połączyć się z bazą danych. Error: ' . mysql_error());
-
-			$mydb=mysql_select_db("$DBName");
-
-	
-
+if(isset($_POST['submit'])){
+	if(isset($_POST['category'])){
+		if(isset($_POST['keyword'])){
 			
-echo "<table>";
+			
+$category = $_POST['category'];
+$keyword = $_POST['keyword'];
+	
+echo"<table>";
 
+switch($_POST['category'])		
+{
+	
+	case "Osoba":
+				$st=$DB->prepare("SELECT id, imie, nazwisko, email FROM Osoba WHERE imie LIKE '%" . $keyword . "%' OR nazwisko LIKE '%" . $keyword . "%' OR email LIKE '%" . $keyword . "%'");
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$imie = $row['imie'];
+					$nazwisko = $row['nazwisko'];
+					$email = $row['email'];
+					
+					echo "<tr>";	
+					echo "<td><a  href=\"view_osoba.php?id=$ID\">"  .$ID. "</a></td><td><a  href=\"view_osoba.php?id=$ID\">"  .$imie . " </a></td><td><a  href=\"view_osoba.php?id=$ID\"> " . $nazwisko . "</a></td><td><a  href=\"view_osoba.php?id=$ID\"> " . $email . "</a></td>";
+					echo "</tr>";
+				}
+				break;
+	
+	case "Sprzet":
+				$st=$DB->prepare("SELECT id, nazwa, SUBSTR(opis, 1, 120) AS opis FROM Sprzet WHERE nazwa LIKE '%" . $keyword . "%' OR opis LIKE '%" . $keyword . "%'"); 
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$nazwa = $row['nazwa'];
+					$opis = $row['opis'];
+					
+					echo "<tr>";	
+					echo "<td><a  href=\"view_sprzet.php?id=$ID\">" .$ID. "</a></td><td><a  href=\"view_sprzet.php?id=$ID\">"  .$nazwa . " </a></td><td> " . $opis .  "</td>";
+					echo "</tr>";
+				}
+				break;
+	
+	case "Laboratorium":
+				$st=$DB->prepare("SELECT id, nazwa, zespol FROM Laboratorium WHERE nazwa LIKE '%" . $keyword . "%'"); 
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$nazwa = $row['nazwa'];
+					$zespol = $row['zespol'];
+					
+					echo "<tr>";	
+					echo "<td><a  href=\"view_lab.php?id=$ID\">" .$ID. "</a></td><td><a  href=\"view_lab.php?id=$ID\">"  .$nazwa . " </a></td><td> " . $zespol . " </td>";
+					echo "</tr>";
+				}
+				break;
+				
+	case "Tag":
+				$st=$DB->prepare("SELECT id, nazwa FROM Tag WHERE nazwa LIKE '%" . $keyword . "%'"); 
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$nazwa = $row['nazwa'];
+					
+					echo "<tr>";	
+					echo "<td>" . "<a  href=\"view_tag.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td>";
+					echo "</tr>";
+				}
+				break;
+				
+	case "Zespol":
+				$st=$DB->prepare("SELECT id, nazwa FROM Zespol WHERE nazwa LIKE '%" . $keyword . "%'"); 
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$nazwa = $row['nazwa'];
+					
+					echo "<tr>";	
+					echo "<td><a  href=\"view_zespol.php?id=$ID\">" .$ID. "</a></td><td><a  href=\"view_zespol.php?id=$ID\">"  .$nazwa . "</a></td>";
+					echo "</tr>";
+				}	
+				break;
+				
+	case "Zaklad":
+				$st=$DB->prepare("SELECT id, nazwa FROM Zaklad WHERE nazwa LIKE '%" . $keyword . "%'"); 
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$nazwa = $row['nazwa'];
+					
+					echo "<tr>";	
+					echo "<td><a  href=\"view_zaklad.php?id=$ID\">" .$ID. "</a></td><td><a  href=\"view_zaklad.php?id=$ID\">"  .$nazwa . "</a></td>";
+					echo "</tr>";
+				}
+				break;
+				
+	case "Projekt":
+				$st=$DB->prepare("SELECT id, nazwa, opis FROM Projekt WHERE nazwa LIKE '%" . $keyword . "%' OR opis LIKE '%" . $keyword . "%'"); 
+				$st->execute();
+				while($row=$st->fetch(PDO::FETCH_ASSOC))
+				{
+					$ID = $row['id'];
+					$nazwa = $row['nazwa'];
+					$opis = mysql_query("SELECT LEFT(opis, 40) FROM Sprzet WHERE id = '$ID'");
+					
+					echo "<tr>";	
+					echo "<td><a  href=\"view_projekt.php?id=$ID\">" .$ID. "</a></td><td><a  href=\"view_projekt.php?id=$ID\">"  .$nazwa . "</a></td><td> " . $opis .  "</td>";
+					echo "</tr>";
+				}
+				break;
 
-if($cat == "Sprzet")
-{ 
-$sql="SELECT id, nazwa, opis, laboratorium FROM $cat WHERE nazwa LIKE '%" . $keyword . "%' OR opis LIKE '%" . $keyword . "%'"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$nazwa = $row['nazwa'];
-		$opis = mysql_query("SELECT LEFT(opis, 40) FROM Sprzet WHERE id = '$ID'");
-		$lab = $row['laboratorium'];
-
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td><td> " . $opis .  "</td>";
-		echo "</tr>";
-	}
 
 }
-
-
-if($cat == "Osoba")
-{ 
-$sql="SELECT id, imie, nazwisko, email FROM $cat WHERE imie LIKE '%" . $keyword . "%' OR nazwisko LIKE '%" . $keyword . "%' OR email LIKE '%" . $keyword . "%'"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$imie = $row['imie'];
-		$nazwisko = $row['nazwisko'];
-		$email = $row['email'];
-
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$imie . " </td><td> " . $nazwisko . " </td><td> " . $email . "</td>";
-		echo "</tr>";
-	}
-
-}
-
-
-if($cat == "Laboratorium")
-{ 
-$sql="SELECT id, nazwa, zespol FROM $cat WHERE nazwa LIKE '%" . $keyword . "%"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$nazwa = $row['nazwa'];
-		$zespol = $row['zespol'];
-
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td><td> " . $zespol . " </td>";
-		echo "</tr>";
-	}
-}
-
-
-if($cat == "Tag")
-{ 
-$sql="SELECT id, nazwa FROM $cat WHERE nazwa LIKE '%" . $keyword . "%'"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$nazwa = $row['nazwa'];
-		
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td>";
-		echo "</tr>";
-	}
-}
-
-
-if($cat == "Zespol")
-{ 
-$sql="SELECT id, nazwa FROM $cat WHERE nazwa LIKE '%" . $keyword . "%'"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$nazwa = $row['nazwa'];
-		
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td>";
-		echo "</tr>";
-	}
-}
-
-
-if($cat == "Zaklad")
-{ 
-$sql="SELECT id, nazwa FROM $cat WHERE nazwa LIKE '%" . $keyword . "%'"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$nazwa = $row['nazwa'];
-		
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td>";
-		echo "</tr>";
-	}
-}
-
-
-if($cat == "Projekt")
-{ 
-$sql="SELECT id, nazwa, opis FROM $cat WHERE nazwa LIKE '%" . $keyword . "%' OR opis LIKE '%" . $keyword . "%'"; 
-$result=mysql_query($sql);
-
-while($row=mysql_fetch_array($result))
-	{
-		$ID = $row['id'];
-		$nazwa = $row['nazwa'];
-		$opis = mysql_query("SELECT LEFT(opis, 40) FROM Sprzet WHERE id = '$ID'");
-
-		echo "<tr>";	
-  		echo "<td>" . "<a  href=\"search.php?id=$ID\">" .$ID. "</a></td><td>"  .$nazwa . " </td><td> " . $opis .  "</td>";
-		echo "</tr>";
-	}
-}
-
 
 echo "</table>";
-		}
-  		else{ echo "Wprowadź zapytanie"; }
-  	}
-  }
-?>
 
+}else{echo "Niepoprawne zapytanie";}
+}else{echo "Niepoprawna kategoria";}
+}
+
+?>
 
 </div>
 </body>
 </html>
-
